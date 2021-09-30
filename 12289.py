5a312a359ac800deca2f2d9958d242b0c5ae1c68
Nahidjc <nahidjc80@gmail.com>
2021-09-30 21:35:08 +0600

    12289 One-Two-Three

--- 12289.py
+++ 12289.py
@@ -1,10 +1,22 @@
-words=['one','two','three','four','five','six','seven','eight','nine','ten']
-
+words=['one','two','three']
 n= int(input())
 i=0
-typeWords=[]
-while i<n:
-    m=input()
-    typeWords.append(m)
-    i=i+1
-print()
+if n<=10:
+    while i<n:
+        m=input()
+        j=0
+        while j<3:
+             if len(words[j])==len(m):
+                 counter = 0
+                 k = 0
+                 while k < len(m):
+                     if m[k] != words[j][k]:
+                         counter += 1
+                     k = k + 1
+                 if counter < 2:
+                    print(j+1)
+                    break
+
+             j=j+1
+        i=i+1
+
